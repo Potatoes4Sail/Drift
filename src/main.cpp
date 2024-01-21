4dc3cb397c3fac12416bbeaa95ae7e982d38a69c
Potatoes4Sail <72229773+Potatoes4Sail@users.noreply.github.com>
2024-01-21 01:29:06 -0500

    Add code to flash LED for testing purposes

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
 void setup() {
-// write your initialization code here
+    // write your initialization code here
+    pinMode(LED_BUILTIN, OUTPUT); // Uses built in variable to set LED
 }
 
 void loop() {
 // write your code here
-// test,should fail
-    printf("Hi");
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
 }
