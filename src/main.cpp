a52b0e60cea892ce4cc9d5db28707295955b0709
Potatoes4Sail <72229773+Potatoes4Sail@users.noreply.github.com>
2024-01-21 01:41:29 -0500

    Modify code to make it easier to verify that you can flash the board. cycle time is now variable

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+
 void setup() {
     // write your initialization code here
     pinMode(LED_BUILTIN, OUTPUT); // Uses built in variable to set LED
@@ -6,8 +7,10 @@ void setup() {
 
 void loop() {
 // write your code here
+    static const int timeMs = 1200; //  Allow for easily verifying you can compile & upload code
+
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
+    delay(timeMs);
     digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+    delay(timeMs);
 }
