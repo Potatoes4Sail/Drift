f87470e93c07e40c301fdcb63766e2e311bbdd77
Patrick <72229773+Potatoes4Sail@users.noreply.github.com>
2024-01-21 13:25:52 -0500

    Add ability to build for Arduno Uno (#2)
    * Add option to build for uno as well
    
    * Add code to flash LED for testing purposes
    
    * Modify code to make it easier to verify that you can flash the board. cycle time is now variable

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <Arduino.h>
+
 void setup() {
-// write your initialization code here
+    // write your initialization code here
+    pinMode(LED_BUILTIN, OUTPUT); // Uses built in variable to set LED
 }
 
 void loop() {
 // write your code here
-// test,should fail
+    static const int timeMs = 1200; //  Allow for easily verifying you can compile & upload code
 
-    printf("Hi");
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(timeMs);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(timeMs);
 }
