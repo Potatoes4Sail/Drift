ef33a199cdc26c5d9a943a8966fa19b69e2a7e62
Patrick <72229773+Potatoes4Sail@users.noreply.github.com>
2024-02-16 09:57:01 -0500

    Code written for ultrasonic driver

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,45 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <HardwareSerial.h>
+
+
+void triggerUltrasound();
 
 int main(void) {
+    Serial.begin(9600);
     DDRB |= _BV(DDB5); // Set LED as an output
+    DDRB |= _BV(DDB1); // Set trigger to output
+    DDRB &= ~_BV(DDB2); // Set echo to input
+    DDRB &= ~_BV(DDB3);
 
     while (1) {
         PORTB |= _BV(PORTB5);
-        _delay_ms(10);
-        PORTB &= ~_BV(PORTB5);
         _delay_ms(100);
+        PORTB &= ~_BV(PORTB5);
+        _delay_ms(10);
+        triggerUltrasound();
+
+        //Serial.println("sent a pulse");
+
+        while (true) {
+            if(_BV(PINB2) & PINB){
+                Serial.println("hi");
+                for (int i = 0; i < 10; ++i) {
+                    PORTB ^= _BV(PORTB5);
+                    _delay_ms(50);
+                }
+                break;
+            }
+        }
     }
     return 1;
+}
+
+
+void triggerUltrasound() {
+    PORTB &= ~_BV(DDB1); // Turns it off
+    _delay_us(10);
+    PORTB |= _BV(DDB1);
+    _delay_us(10);
+    PORTB &= ~_BV(DDB1);
 }
