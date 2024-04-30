4d321ff541ff2366f6368369fda3dd580b56d5d8
Ruben1729 <ruben.e.sanchez.a@gmail.com>
2024-04-30 12:12:31 -0400

    PID controller

--- src/bst7960Driver.cpp
+++ src/bst7960Driver.cpp
@@ -3,16 +3,13 @@
 //
 
 #include <HardwareSerial.h>
+#include <Arduino.h>
 #include "bst7960Driver.h"
 
+#define KP 50.0f
+#define KI 20.0f
+#define KD 3.0f
 
-/// Constructor for class which drives the L298 motor drivers.
-/// \n LIMITATIONS: Both pwm signals must come from the same timer, and TIMER0 is currently chosen.
-/// Does not validate if the two motors are on the two outputs of the timers
-/// \param pwmPin - Pin used for PWMing the enable (MUST BE ONE OF TWO)
-/// \param forwardPWMPin - Any GPIO for controlling forward pin
-/// \param reversePWMPin - Any GPIO for controlling reverse pin
-/// \param deadband - Amount #/255 below which no response will occur from motor. Used as there is likely minimum pwm for anything to occur
 BST7960Driver::BST7960Driver(uint8_t forwardPWMPin, uint8_t reversePWMPin, uint8_t deadbandVal) {
 
     // Sets variables of the class.
@@ -34,18 +31,41 @@ BST7960Driver::BST7960Driver(uint8_t forwardPWMPin, uint8_t reversePWMPin, uint8
             reversePWMPin);      // Sets PWM pin to output
 }
 
-/// Function with sets the speed of the motor
-/// \param speedVar - int8_t value, must be between -127 and 127
-/// \return
 int BST7960Driver::setSpeed(int8_t speedVar) {
-    this->direction = ((0 < speedVar) ? FORWARD : REVERSE);
-    this->speed = 2 * abs(speedVar);
+    uint8_t actualSpeed = 2 * abs(speedVar);
 
-    if (speed < deadband) {
-        this->direction = NONE;
-        this->speed = 0;
-    }
-    setRegisterSpeed(direction);
+    unsigned long now = micros();
+    double dt = (now - lastTime) / 1000000.0;
+    if (dt == 0) return 0;
+
+    // MEASURE SPEED AND COMPUTE ERROR
+    // measuredSpeed = getMeasuredSpeed();
+    float measuredSpeed = 0;
+    static float error = actualSpeed - measuredSpeed;
+    // We should do the difference in expected angular velocity and measured in RPM
+
+    // Calculate PID
+    integral += error * dt;
+    float derivative = error - lastError;
+    float output = (KP * error) + (KI * integral) + (KD * derivative);
+
+    lastError = error;
+    lastTime = now;
+
+    // Saturate
+    if(output < 0) output = 0;
+    else if(output > 255) output = 255;
+    setRegisterSpeed((uint8_t)output);
+
+    // OLD CODE
+//    this->direction = ((0 < speedVar) ? FORWARD : REVERSE);
+//    this->speed = 2 * abs(speedVar);
+//
+//    if (speed < deadband) {
+//        this->direction = NONE;
+//        this->speed = 0;
+//    }
+//    setRegisterSpeed(direction);
     return 0;
 }
 //    // TODO: Would ideally set both pins at the same time to avoid different states,
